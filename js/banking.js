59b0c6c8a138a06a3c83717519c198f7a704640d
sabbir880 <drsabbir880@gmail.com>
2022-03-12 12:50:41 +0600

    updated balance

--- js/banking.js
+++ js/banking.js
@@ -1,12 +1,21 @@
 document.getElementById('deposite-btn').addEventListener('click' , function(){
     const depositeInput = document.getElementById('deposite-input');
-    const depositeAmount = depositeInput.value;
+    const depositeAmountText = depositeInput.value;
+    const depositeAmount =parseFloat(depositeAmountText);
     console.log(depositeAmount)
 
     // get privious deposite amount 
     const depositeTotal = document.getElementById('deposite-total');
     const depsiteTotaltext = depositeTotal.innerText;
-    depositeTotal.innerText = depositeAmount;
+    const priviousDepositeAmount =parseFloat(depsiteTotaltext)
+    depositeTotal.innerText = priviousDepositeAmount + depositeAmount;
+
+    // updated blance 
+    const balancTotal  = document.getElementById('balanc-total');
+    const balanceTotalText = balancTotal.innerText;
+    const priviousBalanceTotal =parseFloat(balanceTotalText);
+
+    balancTotal.innerText = priviousBalanceTotal + depositeAmount;
 
     // clear deposite field 
     depositeInput.value = '';
